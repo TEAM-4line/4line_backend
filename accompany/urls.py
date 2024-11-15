5c46a6fde8475ad96bdf325f16fd9b6f55d7e21b
SEOMUNJI <tjanswl@naver.com>
2024-11-16 03:53:22 +0900

    게시물 상세 조회

--- accompany/urls.py
+++ accompany/urls.py
@@ -11,21 +11,13 @@ default_router = routers.SimpleRouter(trailing_slash=True)
 default_router.register("", AccompanyViewSet, basename="accompanies")
 
 urlpatterns = [
-    # Accompany filtering by trip_type with trailing slash
+    # 여행 타입별 게시물 조회
     path("<str:trip_type>/", AccompanyViewSet.as_view({'get': 'list'}), name="accompany_by_type"),
-    
+
     # Base AccompanyViewSet URLs
     path("", include(default_router.urls)),
 
-    # Nested comments for a specific Accompany with trailing slash
-    path(
-        "<int:accompany_id>/comments/",
-        AccompanyCommentViewSet.as_view({'get': 'list', 'post': 'create'}),
-        name="accompany_comments",
-    ),
-    path(
-        "<int:accompany_id>/comments/<int:pk>/",
-        CommentViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}),
-        name="delete_comment",
-    )
+   #댓글 관련
+    path("<int:accompany_id>/comments/", AccompanyCommentViewSet.as_view({'get': 'list', 'post': 'create'}), name="accompany_comments"),
+    path( "<int:accompany_id>/comments/<int:pk>/", CommentViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name="delete_comment")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
