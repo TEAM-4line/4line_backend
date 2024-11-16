984042e61d040b6d404b46b22d918c813c4a3642
SEOMUNJI <tjanswl@naver.com>
2024-11-16 11:34:50 +0900

    커뮤니티 권한 수정

--- community/models.py
+++ community/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.conf import settings
+
 class Community(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
